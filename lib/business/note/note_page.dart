78bb989dd99da4516043d9eec5edf8f8c6be6144
darren-you <darren-you@outlook.com>
2024-06-13 18:18:30 +0800

    修改Note页面背景色

--- lib/business/note/note_page.dart
+++ lib/business/note/note_page.dart
@@ -10,7 +10,7 @@ class NotePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      //color: Colors.white,
       child: ListView(
         children: [
           const SizedBox(height: 50),
